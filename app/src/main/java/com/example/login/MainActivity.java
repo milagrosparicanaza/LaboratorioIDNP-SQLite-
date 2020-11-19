6f81e5a2860b37a5f4d3a853b7138705fe3303ac
mparicanzac <mparicanazac@unsa.edu.pe>
2020-11-19 01:50:59 -0500

    Mejoras de la interfaz (mejor distribución, cambio de colores)

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -5,23 +5,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    TextView tv_registrar;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText user, pass;
+    Button btnEntrar, btnRegistrar;
+    daoUsuario dao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv_registrar = findViewById(R.id.tv_registar);
+        user = (EditText) findViewById(R.id.TextV_usu);
+        pass = (EditText) findViewById(R.id.TextV_pass);
+        btnEntrar = (Button) findViewById(R.id.Btn_iniciar);
+        btnRegistrar = (Button) findViewById(R.id.Btn_registar);
 
-        tv_registrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentReg = new Intent(MainActivity.this, Register.class);
-                MainActivity.this.startActivity(intentReg);
-            }
-        });
+        btnEntrar.setOnClickListener(this);
+        btnRegistrar.setOnClickListener(this);
+        dao = new daoUsuario(this);
+    }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.Btn_iniciar:
+                String u=user.getText().toString();
+                String p = pass.getText().toString();
+                if(u.equals("") && p.equals("")){
+                    Toast.makeText(this,"ERROR: Campos vacios", Toast.LENGTH_LONG).show();
+                }else if(dao.login(u,p)==1){
+                    Usuario ux= dao.getUsuario(u,p);
+                    Toast.makeText(this,"Datos correctos", Toast.LENGTH_LONG).show();
+                    Intent i2= new Intent(MainActivity.this, Usuario.class);
+                    i2.putExtra("id", ux.getId());
+                    startActivity(i2);
+                }
+                break;
+            case R.id.Btn_registar:
+                Intent i= new Intent(MainActivity.this, Register.class);
+                startActivity(i);
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/login/Register.java
+++ app/src/main/java/com/example/login/Register.java
@@ -2,13 +2,59 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Register extends AppCompatActivity {
+public class Register extends AppCompatActivity implements View.OnClickListener{
+EditText us, pas, nom, ap;
+Button reg, can;
+daoUsuario dao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        us = (EditText) findViewById(R.id.TextV_RegUsers);
+        pas = (EditText) findViewById(R.id.TextV_RegPass);
+        nom = (EditText) findViewById(R.id.TextV_RegNom);
+        ap = (EditText) findViewById(R.id.TextV_RegApe);
+        reg = (Button) findViewById(R.id.Btn_RegRegistrar);
+        can = (Button) findViewById(R.id.Btn_RegCancel);
+
+        reg.setOnClickListener(this);
+        can.setOnClickListener(this);
+        dao = new daoUsuario(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.Btn_RegRegistrar:
+                Usuario u = new Usuario();
+                u.setUsuario(us.getText().toString());
+                u.setPassword(pas.getText().toString());
+                u.setNombre(nom.getText().toString());
+                u.setApellidos(ap.getText().toString());
+                if(!u.isNull()){
+                    Toast.makeText(this, "ERROR: Campos vacios", Toast.LENGTH_LONG).show();
+                }else if(dao.insertUsuario(u)){
+                    Toast.makeText(this, "Registro Exitoso!!", Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(Register.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }else{
+                    Toast.makeText(this, "Usuario ya Registrado!!!", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.Btn_RegCancel:
+                Intent i = new Intent(Register.this, MainActivity.class);
+                startActivity(i);
+                finish();
+                break;
+        }
     }
 }
